4a4d022c711c2ebd61b50fbaf4ef6113bcce8486
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-01-29 16:49:50 +0100

    Ejercicio Props 1

---
+++ props/src/App.jsx
@@ -0,0 +1,11 @@
+import { Welcome } from "./Welcome";
+
+export function App() {
+    return(
+        <div>
+            <h1>Learning props.</h1>
+            <hr />
+            <Welcome name="Carlos"/>
+        </div> 
+    )
+}
---
+++ props/src/Welcome.jsx
@@ -0,0 +1,7 @@
+export function Welcome({name}) {
+    return(
+        <p>
+            Welcome, {name? name :"User"}!
+        </p>
+    )
+}

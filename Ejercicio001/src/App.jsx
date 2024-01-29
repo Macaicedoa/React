5a3e21e6e1e10f4697da30376ef66fe658ec8d81
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-01-29 12:45:12 +0100

    Ejercicio003

--- Ejercicio001/src/App.jsx
+++ Ejercicio001/src/App.jsx
@@ -1,10 +1,20 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App(){
     return (
         <div>
             <Hello />
+            <Hello />
+            <Hello />
+            <Hello />
+            <Hello />
+            <Hello />
+            <Message/>
         </div>
     )
 }
 
+// Se puede renderizar un mismo componente tantas veces se necesite, los componentes se crean para poder ser reutilizados.
+// También es posible renderizar componentes directamente en el componente raiz.
+// Siempre y cuando el componente raiz devuelva un solo contenedor o elemento padre.

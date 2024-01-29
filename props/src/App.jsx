0198dbe64e4a9c2d822b6c973037ecd18eabf9e4
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-01-29 16:52:46 +0100

    Ejercicio Props 1

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -5,7 +5,9 @@ export function App() {
         <div>
             <h1>Learning props.</h1>
             <hr />
-            <Welcome name="Carlos"/>
+            <Welcome name="María"/>
         </div> 
     )
 }
+
+// Si no le paso el valor de name a <Welcome/> solo renderizará una parte de la frase dentro de <p/>: "Welcome, !"

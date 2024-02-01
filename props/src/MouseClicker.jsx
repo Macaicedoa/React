1a579894b5d7055d5d48250b4f8063e084d17f8a
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-01 17:40:12 +0100

    Handling Events -2

--- props/src/MouseClicker.jsx
+++ props/src/MouseClicker.jsx
@@ -2,11 +2,15 @@ function MouseClicker() {
   
 	function handleButtonClick(event) {
         //console.log(event)
-				console.log(event.target.name)
+				//console.log(event.target.src)
+				console.log(event.target == event.currentTarget ? event.target.name : event.target.src)
 	}
 
   return (
-    <button name= "one" onClick={handleButtonClick}>Click me!</button>
+    <button name= "one" onClick={handleButtonClick}>
+			<img src="../public/vite.svg" alt="" width={16} height={16}/>
+			Click me!
+			</button>
   )
 }
 

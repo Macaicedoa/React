d0e8f7b205c5a8a9fcd502674e8d515950c88b2b
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-02 12:40:25 +0100

    Controlled Forms -2

--- props/src/AppForms.jsx
+++ props/src/AppForms.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import InteractiveWelcome from './InteractiveWelcome'
+import Login from './Login'
 
 function AppForms() {
   return (
     <div>
         <h1>Learning React.</h1>
+        <h2> - Forms -</h2>
         <hr /> 
         <InteractiveWelcome/>
+        <hr />
+        <h2>Login</h2>
+        <Login/>
     </div>
   )
 }
---
+++ props/src/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+function Login() {
+  
+	const [data, setData] = useState({
+		username:"",
+		password:"",
+		remember: false,
+	})
+	
+	function handleInputChange(event) {
+		const name = event.target.name
+		const type = event.target.type
+		const value = event.target.value
+		const checked = event.target.checked
+
+		setData(()=>{
+			return {...data,[name]:(type!="checkbox"?value:checked)}
+		})
+
+		console.log(data)
+	}
+	
+  return (
+		<div>
+			<label htmlFor="username">User:</label>
+			<input name="username" value={data.username} type="text" onChange={handleInputChange}/>
+			<br />
+			<label htmlFor="password">Password</label>
+			<input type="password" value={data.password} name="password" onChange={handleInputChange}/>
+			<br />
+			<label htmlFor="remember">Remember:</label>
+			<input type="checkbox" checked={data.remember} name="remember" onChange={handleInputChange}/>
+		</div>
+  )
+}
+
+export default Login

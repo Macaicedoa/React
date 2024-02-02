5be9369f9f86d9ff4467db3bbe481aa000dcbbd5
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-02 10:10:35 +0100

    Corrección handle 2

--- props/src/MouseClicker.jsx
+++ props/src/MouseClicker.jsx
@@ -1,19 +1,17 @@
 function MouseClicker() {
   
 	function handleButtonClick(event) {
-        //console.log(event)
-				//console.log(event.target.src)
-				//console.log(event.target == event.currentTarget ? event.target.name : event.target.src)
 				console.log(event.target.name)
-				
-
-				console.log(event.target.src)
-				event.stopPropagation();
 	}
+
+	function handleimageClick(event) {
+		event.stopPropagation();
+		console.log(event.target.src)	
+}
 
   return (
     <button name= "one" onClick={handleButtonClick}>
-			<img src="../public/vite.svg" alt="" width={16} height={16}/>
+			<img onClick={handleimageClick} src="../public/vite.svg" alt="" width={16} height={16}/>
 			Click me!
 		</button>
   )

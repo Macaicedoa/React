6de423714789142a0c55ded5935197c9be2d21b3
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-03 11:36:23 +0100

    Uncontrolled Forms -2

--- props/src/forms/UncontrolledForm.jsx
+++ props/src/forms/UncontrolledForm.jsx
@@ -11,8 +11,20 @@ function UncontrolledForm() {
 			password,
 			session
 		}
-
+		
 		console.log(data)
+
+		const myFormData = new FormData(event.target)
+
+		console.log(myFormData.get('username'),myFormData.get('password'))
+
+		/*Advantages:
+		 There’s less boilerplate when there are many inputs and we don’t need to manage multiple hook calls or a large complex state object.
+		 Disadvantages:
+		 We don’t have direct access to each input’s value. This can make custom validation more difficult, and makes direct input value manipulation impossible 
+		 (like disabling buttons or checking passwords)
+		 */
+
 	}
 
   return (

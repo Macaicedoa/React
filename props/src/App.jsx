d4f5f99f6325b067a79a06dab55da4643649a69e
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-12 11:37:21 +0100

    Router -2

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route,Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import {Counter} from "./Counter"
 
 export function App() {
 
     return(
         <Routes>
             <Route path="/" element={<Welcome name="Juan"/>}/>
+            <Route path="/counter" element={<Counter initValue={10}/>}/>
         </Routes>
     )
 }

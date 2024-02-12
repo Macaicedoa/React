5f2dab1afc8ea6e562d94660d67a8f4b7f1dd823
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-12 17:27:00 +0100

    Router -4

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route,Routes } from "react-router-dom";
+import { Link, Route,Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import {Counter} from "./Counter"
 import ShowGithubUser from "./ShowGithubUser";
@@ -6,11 +6,18 @@ import ShowGithubUser from "./ShowGithubUser";
 export function App() {
 
     return(
-        <Routes>
-            <Route path="/" element={<Welcome name="Juan"/>}/>
-            <Route path="/counter" element={<Counter initValue={10}/>}/>
-            <Route path="/users/:username" element={<ShowGithubUser/>}/>
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Welcome name="Juan"/>}/>
+                <Route path="/counter" element={<Counter initValue={10}/>}/>
+                <Route path="/users/:username" element={<ShowGithubUser/>}/>
+            </Routes>
+            <div className="links-to">
+                <Link to="/" >Volver al inicio</Link>
+                <Link to="/counter" >Ver Contador</Link>
+                <Link to="/users/macaicedoa">Ver GitHub User</Link>
+            </div>
+        </>
     )
 }
 

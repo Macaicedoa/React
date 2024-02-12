0536289a906e012d4fdcc04043dbb212035ad932
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-12 17:44:12 +0100

    Router -5

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -2,21 +2,24 @@ import { Link, Route,Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import {Counter} from "./Counter"
 import ShowGithubUser from "./ShowGithubUser";
+import NotFound from "./NotFound";
 
 export function App() {
 
     return(
         <>
-            <Routes>
-                <Route path="/" element={<Welcome name="Juan"/>}/>
-                <Route path="/counter" element={<Counter initValue={10}/>}/>
-                <Route path="/users/:username" element={<ShowGithubUser/>}/>
-            </Routes>
             <div className="links-to">
                 <Link to="/" >Volver al inicio</Link>
                 <Link to="/counter" >Ver Contador</Link>
                 <Link to="/users/macaicedoa">Ver GitHub User</Link>
             </div>
+            <Routes>
+                <Route path="/" element={<Welcome name="Juan"/>}/>
+                <Route path="/counter" element={<Counter initValue={10}/>}/>
+                <Route path="/users/:username" element={<ShowGithubUser/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+            
         </>
     )
 }
---
+++ props/src/NotFound.jsx
@@ -0,0 +1,7 @@
+function NotFound() {
+  return (
+    <h1>The page that you are looking for does not exist.</h1>
+  )
+}
+
+export default NotFound

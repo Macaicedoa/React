5fd1b4d21717062c00963f4417c8b75cf566e456
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-12 11:33:48 +0100

    Router-1

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,52 +1,12 @@
-import { AlertClock } from "./AlertClock";
-import Clock from "./Clock";
-import Colors from "./Lists/Colors";
-import { Counter } from "./Counter";
-import MouseClicker from "./MouseClicker";
-import MyForm from "./MyForm";
+import { Route,Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
-import Container from "./Container";
-import { LanguageContext } from "./LanguageContext";
-import { useState } from "react";
-import GithubUser from "./GithubUser";
-import GithubUsers from "./GithubUsers";
-
 
 export function App() {
 
-    const [language, setLanguage] = useState('en')
-
-    function handletSetLanguage(language) {
-        console.log(language)
-        setLanguage(language)
-    }
-
-    // function handleShowCurrentTime() {
-    //     const currentTime = new Date()
-
-    //     alert(`The current time is: ${currentTime.toLocaleTimeString()}`)
-    // }
-
     return(
-        <Container title={<h1>Learning React.</h1>}>
-            <hr />
-            <Welcome name="John" age={10} />
-            {/* <GithubUser username={"macaicedoa"}/> */}
-            <GithubUsers/>
-            <hr />
-            <LanguageContext.Provider value={language}>
-                <select value={language} onChange={(e)=>handletSetLanguage(e.target.value)}>
-                    <option value="es">ES</option>
-                    <option value="en">EN</option>
-                </select>
-                <Clock/>
-
-            </LanguageContext.Provider>
-            <hr />
-            <MouseClicker />
-            <hr />
-            <MyForm/>
-        </Container>  
+        <Routes>
+            <Route path="/" element={<Welcome name="Juan"/>}/>
+        </Routes>
     )
 }
 
--- props/src/index.jsx
+++ props/src/index.jsx
@@ -1,24 +1,18 @@
 import { StrictMode } from "react";
 import { App } from "./App";
 import { createRoot } from 'react-dom/client'
-import AppForms from "./forms/AppForms";
-import References from "./References";
-import AppColors from "./Lists/AppColors";
-import AppTodoList from "./Lists/AppTodoList";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./clock-style.scss"
-import AppHooks from "./hooks/AppHooks";
+
 
 createRoot(document.getElementById('root')).render( 
     <> {/*Etiquetas vacías para no añadir divs*/}
     {/* <StrictMode> */}
-
-        {/* <App /> */}
-        {/* <AppForms/> */}
-        {/* <References/> */}
-        {/* <AppColors/> */}
-         {/* <AppTodoList/> */}
-         <AppHooks/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+         
     {/* </StrictMode>   */}
     
     </>
